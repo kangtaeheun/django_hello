ba86c3b0847a837a35c6dbe39983f27569c9963e
kangtaeheun <kwtsun6111@naver.com>
2021-01-26 17:32:23 +0900

    21/01/26
    1. view를 클래스 제너릭 형태로 작성
    응답 가능한 html 정의

--- todo_subject/todo_board/views.py
+++ todo_subject/todo_board/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views import generic
+from django.views import View
+
+class Todo_board(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'todo_main/index.html'
+        return render(request, template_name)
