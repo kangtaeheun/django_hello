205e9f4ed606ba22f1561a4055c7b955e17ee8a3
kangtaeheun <kwtsun6111@naver.com>
2021-01-27 18:02:35 +0900

    21/01/27 - DB 연결 후 데이터 조회 - 저장하기 기능 추가 - 상세보기 기능 추가

--- todo_subject/todo_board/urls.py
+++ todo_subject/todo_board/urls.py
@@ -8,6 +8,10 @@ app_name='todo_board'
 
 urlpatterns=[
     url(r'^$', views.Todo_board.as_view(), name='todo_board'),
+    url(r'^insert/$', views.check_post, name='todo_board_insert'),
+    # pk는 게시판 고유 번호 즉, 고유번호/detail url로 들어가면 고유번호에 따른 게시판을 상세보기로 볼 수 있음.
+    url(r'^(?P<pk>[0-9]+)/detail/$', views.Todo_board_detail.as_view(), name='todo_board_detail'),
+    #url(r'^board/insert/$', views.Todo_insert.as_View(), name='todo_board'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- todo_subject/todo_board/views.py
+++ todo_subject/todo_board/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, redirect
-
-# Create your views here.
 from django.views import generic
-from django.views import View
+from .models import TodoList
+from .forms import TodoForm
 
+# board view
 class Todo_board(generic.TemplateView):
     def get(self, request, *args, **kwargs):
-        template_name = 'todo_main/index.html'
-        return render(request, template_name)
+        # todo_main에 있는 템플릿 폴더에서 해당 html을 찾으시오.
+        template_name = 'todo_board/todo_board_list.html'
+        # 모든 객체를 가져옴.
+        todo_list = TodoList.objects.all()
+        # 그 결과값을 {'todo_list':todo_list}에 담아서 template로 넘겨줌.
+        return render(request, template_name, {'todo_list': todo_list})
+
+# generic.DetailView를 이용하면 굉장히 편하게 상세보기 페이지를 만들 수 있음.
+class Todo_board_detail(generic.DetailView):
+    model = TodoList
+    template_name = 'todo_board/todo_board_detail.html'
+    context_object_name = 'todo_list' # context_object_name은 해당 object의 이름을 설정. template에서 저 이름을 가지고 접
+
+def check_post(request):
+    template_name = 'todo_board/todo_board_success.html'
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            message = "일정을 추가하였습니다."
+            todo.todo_save()
+            return render(request, template_name, {"message": message})
+    else:
+        template_name = 'todo_board/todo_board_insert.html'
+        form = TodoForm
+        return render(request, template_name, {'form' : form})
