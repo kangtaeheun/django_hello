7ff36b1aa87efcf0dbcccba46d5d80e33fc6e791
kangtaeheun <kwtsun6111@naver.com>
2021-01-29 14:01:25 +0900

    21/01/29
    - 날짜별 게시글

--- todo_subject/todo_board/forms.py
+++ todo_subject/todo_board/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import TodoList
 
+# date input type을 date로 설정 --> 달력 뜨게함.
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class TodoForm(forms.ModelForm):
     class Meta:
         model = TodoList
         fields = ('title', 'content', 'end_date')
+        widgets = {
+            # end_date ==> dateInput 클래스 불러옴.
+            'end_date' : DateInput()
+        }
--- todo_subject/todo_board/views.py
+++ todo_subject/todo_board/views.py
@@ -2,16 +2,31 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from .models import TodoList
 from .forms import TodoForm
+from datetime import datetime
 
 # board view
 class Todo_board(generic.TemplateView):
     def get(self, request, *args, **kwargs):
         # todo_main에 있는 템플릿 폴더에서 해당 html을 찾으시오.
         template_name = 'todo_board/todo_board_list.html'
-        # 모든 객체를 가져옴.
-        todo_list = TodoList.objects.all()
-        # 그 결과값을 {'todo_list':todo_list}에 담아서 template로 넘겨줌.
-        return render(request, template_name, {'todo_list': todo_list})
+        # 기한이 없는 일정, 마감 안된 일정 --> 모든 객체를 가져옴.
+        todo_list_no_endDate = TodoList.objects.all().filter(end_date__isnull=True, is_complete=0).order_by('priority')
+        #기한 있고, 마감이 안된 애들
+        todo_list_endDate_non_complete = TodoList.objects.all().filter(end_date__isnull=False, is_complete=0).order_by('priority')
+        #마김이 된 애들
+        todo_list_endDate_complete = TodoList.objects.all().filter(is_complete=1).order_by('end_date')
+        today = datetime.now()
+        # 마감이 가까운 일정
+        close_end_day = []
+        # 마감이 지난 일정
+        over_end_day = []
+        for i in todo_list_endDate_non_complete:
+            e_day = str(i.end_date).split("-")
+            end_day = datetime(int(e_day[0]), int(e_day[1]), int(e_day[2]))
+            if (end_day - today).days < -1: over_end_day.append(i.title)
+            if (end_day - today).days >= -1 and (end_day - today).days < 3: close_end_day.append(i.title)
+        return render(request, template_name, {"todo_list_endDate_non_complete": todo_list_endDate_non_complete, "todo_list_endDate_complete": todo_list_endDate_complete, "todo_list_no_endDate": todo_list_no_endDate, 'close_end_day': close_end_day, 'over_end_day':over_end_day})
+
 
 # generic.DetailView를 이용하면 굉장히 편하게 상세보기 페이지를 만들 수 있음.
 class Todo_board_detail(generic.DetailView):
